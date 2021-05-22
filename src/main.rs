53c69db740ae9f7377f9835a9a3a616180897280
k-nasa <htilcs1115@gmail.com>
2021-05-23 08:35:51 +0900

    fix bug

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use wasmi::*;
 
 fn main() -> anyhow::Result<()> {
     let args: Vec<String> = std::env::args().collect();
-    if args.is_empty() {
+    if args.len() < 2 {
         anyhow::bail!("Plese speficy filepath")
     }
 
@@ -10,9 +10,9 @@ fn main() -> anyhow::Result<()> {
     let bytes = std::fs::read(filename)?;
 
     let m = Module::from_byte(bytes)?;
-    dbg!(m);
-    // let instance = Instance::new(m);
-    // instance.invoke("hoge")?;
+    dbg!(&m);
+    let instance = Instance::new(m);
+    instance.invoke("add")?;
 
     Ok(())
 }
