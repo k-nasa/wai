a5ac19d6dba8e4e54d134ee8f7dbb5040e6d5b71
k-nasa <htilcs1115@gmail.com>
2021-05-26 08:06:18 +0900

    implement resolve function

--- src/instance/mod.rs
+++ src/instance/mod.rs
@@ -10,7 +10,52 @@ impl Instance {
         Self { module }
     }
 
-    pub fn invoke(&self, _name: impl AsRef<str>) -> std::io::Result<()> {
+    pub fn invoke(&self, name: impl AsRef<str>) -> Result<(), RuntimeError> {
+        let index = self.resolve_function_name(name.as_ref());
+
+        if index.is_none() {
+            return Err(RuntimeError::NotFound(name.as_ref().to_string()));
+        }
+
         todo!()
+    }
+
+    fn resolve_function_name(&self, name: impl AsRef<str>) -> Option<usize> {
+        let export_section = &self.module.export_section;
+
+        let exports = match export_section {
+            None => return None,
+            Some(e) => &e.entries,
+        };
+
+        let entry = exports.iter().find(|x| &x.field_str == name.as_ref());
+
+        entry.and_then(|x| Some(x.index as usize))
+    }
+}
+
+use std::error::Error;
+use std::fmt::{self, Display};
+
+#[derive(Debug)]
+pub enum RuntimeError {
+    NotFound(String),
+    IOError(std::io::Error),
+}
+
+impl Error for RuntimeError {}
+impl Display for RuntimeError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        use self::RuntimeError::*;
+        match self {
+            NotFound(name) => write!(f, "'{}'' is not found", name),
+            IOError(i) => write!(f, "io error: {}", i),
+        }
+    }
+}
+
+impl From<std::io::Error> for RuntimeError {
+    fn from(error: std::io::Error) -> Self {
+        Self::IOError(error)
     }
 }
