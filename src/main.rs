2e4605586c76a061af4134f965c6b11ec0decfbe
k-nasa <htilcs1115@gmail.com>
2021-05-29 18:31:41 +0900

    Add log

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use wasmi::*;
 
 fn main() -> anyhow::Result<()> {
+    std::env::set_var("RUST_LOG", "debug");
+    env_logger::init();
+
     let args: Vec<String> = std::env::args().collect();
     if args.len() < 2 {
         anyhow::bail!("Plese speficy filepath")
@@ -10,10 +13,11 @@ fn main() -> anyhow::Result<()> {
     let bytes = std::fs::read(filename)?;
 
     let m = Module::from_byte(bytes)?;
+    log::debug!("module: {:#?}", m);
 
     let instance = Instance::new(m);
     let values = instance.invoke("add", vec![RuntimeValue::I32(1), RuntimeValue::I32(1)])?;
-    println!("return value is {:?}", values);
+    log::info!("return value is {:?}", values);
 
     Ok(())
 }
