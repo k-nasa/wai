1c3f31da95caea360b7681e4bb0a224bc264c20a
k-nasa <htilcs1115@gmail.com>
2021-05-29 18:46:09 +0900

    Add cli interface

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,36 @@
+use clap::{AppSettings, Clap};
 use wasmi::*;
+
+#[derive(Clap)]
+#[clap(
+    version = "0.1.0",
+    author = "k-nasa <htilcs1115@gmail.com>",
+    about = "A simple wasm interpreter"
+)]
+#[clap(setting = AppSettings::ColoredHelp)]
+struct Opts {
+    file_path: String,
+
+    #[clap(short, long)]
+    invoke: String,
+}
 
 fn main() -> anyhow::Result<()> {
     std::env::set_var("RUST_LOG", "debug");
     env_logger::init();
 
-    let args: Vec<String> = std::env::args().collect();
-    if args.len() < 2 {
-        anyhow::bail!("Plese speficy filepath")
-    }
+    let opts: Opts = Opts::parse();
 
-    let filename = &args[1];
+    let filename = opts.file_path;
     let bytes = std::fs::read(filename)?;
 
     let m = Module::from_byte(bytes)?;
     log::debug!("module: {:#?}", m);
 
     let instance = Instance::new(m);
-    let values = instance.invoke("add", vec![RuntimeValue::I32(1), RuntimeValue::I32(1)])?;
+
+    // TODO implement custom argument
+    let values = instance.invoke(opts.invoke, vec![RuntimeValue::I32(1)])?;
     log::info!("return value is {:?}", values);
 
     Ok(())
