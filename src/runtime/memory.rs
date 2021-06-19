f0197829c4d3216447ecd3cb6a70ab1c799004e8
k-nasa <htilcs1115@gmail.com>
2021-06-19 16:10:30 +0900

    refactor

--- src/runtime/memory.rs
+++ src/runtime/memory.rs
@@ -1,6 +1,5 @@
 use crate::from_le::FromLe;
 use crate::runtime::error::RuntimeError;
-use crate::types::RuntimeValue;
 
 pub struct Memory(Vec<u8>);
 
@@ -11,7 +10,7 @@ impl Memory {
 
     pub fn load<T>(&self, addr: usize) -> Result<T, RuntimeError>
     where
-        T: Into<RuntimeValue> + FromLe,
+        T: FromLe,
     {
         let size = std::mem::size_of::<T>();
         let buf = &self.0[addr..addr + size];
